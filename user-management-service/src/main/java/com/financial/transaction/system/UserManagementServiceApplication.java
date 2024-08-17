509a742afa612e3588ecffe501abbee3dccf6c47
Sahil Dhiman <dhiman.sahil03022001@gmail.com>
2024-08-17 12:22:47 +0530

    made 2 microservices

---
+++ user-management-service/src/main/java/com/financial/transaction/system/UserManagementServiceApplication.java
@@ -0,0 +1,11 @@
+package com.financial.transaction.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementServiceApplication.class, args);
+    }
+}
