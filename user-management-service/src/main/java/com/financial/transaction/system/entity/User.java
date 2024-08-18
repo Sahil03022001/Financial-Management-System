30a83acb677f51a7a82adcc14278946c8dbb45b9
Vishal Antil <antilvishal2402@gmail.com>
2024-08-18 23:17:04 +0530

    User added

---
+++ user-management-service/src/main/java/com/financial/transaction/system/entity/User.java
@@ -0,0 +1,45 @@
+package com.financial.transaction.system.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    private String userId = UUID.randomUUID().toString();
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private int mobNo;
+
+    //Format -> yyyy-mm-dd
+    private Date dateOfBirth;
+
+    private String address;
+
+    @CreationTimestamp
+    private Date created;
+
+    private LocalDate updated;
+
+}
