e7b3963713f1084884a98c1bd3aaa8b4eeb9afbe
Sahil Dhiman <dhiman.sahil03022001@gmail.com>
2024-10-05 22:17:16 +0530

    release-kafka-01: added changes for sending mail for user

--- notification-management-service/src/main/java/com/financial/transaction/system/kafka/consumer/KafkaConsumerService.java
+++ notification-management-service/src/main/java/com/financial/transaction/system/kafka/consumer/KafkaConsumerService.java
@@ -2,10 +2,12 @@ package com.financial.transaction.system.kafka.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.financial.transaction.system.entity.User;
+import com.financial.transaction.system.mail.UserDetailUpdateMailService;
 import com.financial.transaction.system.responseDTO.UserResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +19,14 @@ public class KafkaConsumerService {
 
     private final ObjectMapper mapper = new ObjectMapper();
 
-    @KafkaListener(topics = "user-change-topic", groupId = "{spring.kafka.consumer.group-id}")
+    private final String userChangeTopic = "user-change-topic";
+
+    @Autowired
+    UserDetailUpdateMailService userDetailUpdateMailService;
+
+    @KafkaListener(topics = userChangeTopic, groupId = "{spring.kafka.consumer.group-id}")
     public void consume(UserResponseDTO data) {
-        LOG.info("Received Data: {}", data);
+        LOG.info("Received Data for topic: {}, {}", userChangeTopic, data);
+        userDetailUpdateMailService.sendUserDetailUpdateMail(data);
     }
 }
